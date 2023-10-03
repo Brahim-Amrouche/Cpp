18b7e4e3c6d190c74a71a37078150081a3568c0d
Amrouche Brahim <amroucheb4@gmail.com>
2023-10-03 23:11:21 +0100

    cpp06 done and dusted

--- cpp06/ex02/Types.cpp
+++ cpp06/ex02/Types.cpp
@@ -6,14 +6,100 @@
 /*   By: bamrouch <bamrouch@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 14:53:27 by bamrouch          #+#    #+#             */
-/*   Updated: 2023/10/02 14:58:24 by bamrouch         ###   ########.fr       */
+/*   Updated: 2023/10/03 23:08:14 by bamrouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Types.hpp"
 
+
 Base *Base::generate(void)
 {
-       
+    time_t current_time = time(NULL);
+    srand((unsigned int) current_time);
+
+    int randomnum = (rand() % 3);
+    
+    switch (randomnum)
+    {
+        case A_CLASS:
+            return new A();
+        case B_CLASS:
+            return new B();
+        case C_CLASS:
+            return new C();
+        default:
+            break;
+    }
+    return NULL;
 }
+
+void    Base::identify(Base *p)
+{
+    A *a_cast = dynamic_cast<A *>(p);
+    if (a_cast)
+    {
+        cout << "A" << endl;
+        return;
+    }
+    
+    B *b_cast = dynamic_cast<B *>(p);
+    if (b_cast)
+    {
+        cout << "B" << endl;
+        return;
+    }
+
+    C *c_cast = dynamic_cast<C *>(p);
+    if (c_cast)
+        cout << "C" << endl;  
+}
+
+void    Base::identify(Base &p)
+{
+    try {
+        A &a_ref = dynamic_cast<A &>(p);
+        (void) a_ref;
+        cout << "A" << endl;
+        return;
+    }
+    catch(bad_cast &e)
+    {
+        (void) e;
+    }
+
+    try{
+        B &b_ref = dynamic_cast<B &>(p);
+        (void) b_ref;
+        cout << "B" << endl;
+        return ;
+    }
+    catch(bad_cast &e)
+    {
+        (void) e;
+    }
+
+    try{
+        C &c_ref = dynamic_cast<C &>(p);
+        (void) c_ref;
+        cout << "C" << endl;
+        return ;
+    }
+    catch(bad_cast &e)
+    {
+        (void) e;
+    }
+}
+
+Base::~Base()
+{}
+
+A::~A()
+{}
+
+B::~B()
+{}
+
+C::~C()
+{}
 
--- cpp06/ex02/Types.hpp
+++ cpp06/ex02/Types.hpp
@@ -6,18 +6,36 @@
 /*   By: bamrouch <bamrouch@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 14:37:55 by bamrouch          #+#    #+#             */
-/*   Updated: 2023/10/02 14:57:01 by bamrouch         ###   ########.fr       */
+/*   Updated: 2023/10/03 23:04:28 by bamrouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+#include "iostream"
+#include "cstdlib"
+#include "ctime"
+#include "typeinfo"
+
+using std::srand;
+using std::rand;
+using std::time;
+using std::cout;
+using std::endl;
+using std::bad_cast;
+
+enum RANDOM_GENERATION
+{
+    A_CLASS,
+    B_CLASS,
+    C_CLASS
+};
 
 class Base
 {
     public:
-        Base    *generate(void);
-        void     identify(Base *p);
-        void     identify(Base &p);
+        static Base    *generate(void);
+        static void     identify(Base *p);
+        static void     identify(Base &p);
         virtual ~Base();
 };
 
